2ccbd9dae3da53639c34720b5fceb7536b98ebd4
svasenkov <aanher@gmail.com>
2022-08-01 22:14:18 +0300

    added browserstack tests

---
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -0,0 +1,37 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackMobileDriver implements WebDriverProvider {
+
+    @Override
+    public WebDriver createDriver(Capabilities capabilities) {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.merge(capabilities);
+        mutableCapabilities.setCapability("browserstack.user", "dopik_n0Isgm");
+        mutableCapabilities.setCapability("browserstack.key", "ahiaWEYwHmYJWdAWFDBr");
+        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        mutableCapabilities.setCapability("device", "Google Pixel 3");
+        mutableCapabilities.setCapability("os_version", "9.0");
+        mutableCapabilities.setCapability("project", "First Java Project");
+        mutableCapabilities.setCapability("build", "browserstack-build-1");
+        mutableCapabilities.setCapability("name", "first_test");
+
+        return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
+    }
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
